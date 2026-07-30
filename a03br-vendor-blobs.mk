d958bb83502cd24186859baeb136f1d343e8bc07
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-07-30 15:53:49 +0900

    Update a03br-vendor-blobs.mk

--- a03br-vendor-blobs.mk
+++ a03br-vendor-blobs.mk
@@ -1,4 +1,5 @@
 PRODUCT_COPY_FILES += \
+    vendor/sts/a03br/proprietary/lib/libbt-vendor.so:system/lib/libbt-vendor.so \
     vendor/sts/a03br/proprietary/bin/netcfg:system/bin/netcfg \
     vendor/sts/a03br/proprietary/bin/nvm:system/bin/nvm \
     vendor/sts/a03br/proprietary/bin/nvmcp:system/bin/nvmcp \
